5465a7fd18f624ff1c6486c51dffd9a9f52920a6
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-08-11 18:38:16 +0600

    create components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,27 @@
+import Credit from '@/components/Credit'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import Residence from '@/components/Residence'
+import Services from '@/components/Services'
+import Subscription from '@/components/Subscription'
+import Testimonials from '@/components/Testimonials'
+import TopDestination from '@/components/TopDestination'
+import Travel from '@/components/Travel'
 import React from 'react'
 
 const page = () => {
   return (
     <>
-    
+    <Header/>
+    <Hero/>
+    <TopDestination/>
+    <Services/>
+    <Travel/>
+    <Testimonials/>
+    <Subscription/>
+    <Footer/>
+    <Credit/>
     </>
   )
 }
