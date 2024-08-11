5291220954091f9b17ed3bd21b1951664a76cf7b
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-08-11 21:23:31 +0600

    create TopDestination

--- src/components/TopDestination.jsx
+++ src/components/TopDestination.jsx
@@ -4,6 +4,8 @@ import { DestinationData } from "../../Data";
 import Image from "next/image";
 import { IoMdStar } from "react-icons/io";
 import { IoMdStarHalf } from "react-icons/io";
+import greenGrid from "/public/svg/green.svg";
+import orangeGrid from "/public/svg/orange.svg";
 const TopDestination = () => {
   return (
     <section className="container mx-auto xl:my-24">
@@ -13,10 +15,19 @@ const TopDestination = () => {
         headingText={`explore top destination`}
         headingStyle={`my-6 text-5xl`}
       />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      <div className="relative grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        <div className="top-0 absolute -left-5 -z-10">
+          <Image src={orangeGrid} alt="dot grid" />
+        </div>
+        <div className="-bottom-5 absolute -right-5 -z-10">
+          <Image src={greenGrid} alt="dot grid" />
+        </div>
         {DestinationData.map((item, index) => {
           return (
-            <div className="rounded-[2rem] bg-white shadow-lg pb-7 mt-5" key={index}>
+            <div
+              className="rounded-[2rem] bg-white shadow-sm transition-all duration-200 pb-7 cursor-pointer mt-5 hover:shadow-lg"
+              key={index}
+            >
               <Image
                 src={item.image}
                 className="h-64 bg-cover  rounded-tl-[2rem] rounded-tr-[2rem]"
@@ -35,7 +46,9 @@ const TopDestination = () => {
                     <IoMdStarHalf />
                   </div>
                   {/* price */}
-                  <span className="text-orangePrimary font-semibold">{item.price}</span>
+                  <span className="text-orangePrimary text-xl font-semibold">
+                    {item.price}
+                  </span>
                 </div>
               </div>
             </div>
