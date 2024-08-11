9c528dc3d3e8c8cefb87736df5913d08905a7825
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-08-11 18:25:56 +0600

    add assets and fix code

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+      // font family
+      fontFamily:{
+        "poppins":'var(--font-poppins)',
+        "merriweather":'var(--font-merriweather)',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
