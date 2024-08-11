578c88af4148c77530e61e852345e228fd7da288
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-08-11 22:20:33 +0600

    complete services section

--- Data.js
+++ Data.js
@@ -37,34 +37,56 @@ export const HeroFormData = [
     input: "how many peoples",
   },
 ];
-import imageOne from "/public/travel/image-01.jpeg"
-import imageTwo from "/public/travel/image-02.jpeg"
-import imageThree from "/public/travel/image-03.jpeg"
-import imageFour from "/public/travel/image-04.jpeg"
+import imageOne from "/public/travel/image-01.jpeg";
+import imageTwo from "/public/travel/image-02.jpeg";
+import imageThree from "/public/travel/image-03.jpeg";
+import imageFour from "/public/travel/image-04.jpeg";
 
 export const DestinationData = [
   {
-  image:imageOne,
-  title:"Istanbul,Turkey",
-  price:"$750",
-  rating:"",
+    image: imageOne,
+    title: "Istanbul,Turkey",
+    price: "$750",
+    rating: "",
   },
   {
-  image:imageTwo,
-  title:"sent martin,Bangladesh",
-  price:"$640",
-  rating:"",
+    image: imageTwo,
+    title: "sent martin,Bangladesh",
+    price: "$640",
+    rating: "",
   },
   {
-  image:imageThree,
-  title:"Sydney, Australia",
-  price:"$1200",
-  rating:"",
+    image: imageThree,
+    title: "Sydney, Australia",
+    price: "$1200",
+    rating: "",
   },
   {
-  image:imageFour,
-  title:"Cox’s Bazar,Bangladesh",
-  price:"$470",
-  rating:"",
+    image: imageFour,
+    title: "Cox’s Bazar,Bangladesh",
+    price: "$470",
+    rating: "",
+  },
+];
+import { BiSolidPlaneAlt } from "react-icons/bi";
+import { FaBuilding } from "react-icons/fa6";
+import { HiGlobeAsiaAustralia } from "react-icons/hi2";
+
+
+export const ServicesData = [
+  {
+  icon:<BiSolidPlaneAlt />,
+  heading:"choose destination",
+  paragraph:"Work with our travel experts to create a customized itinerary that perfectly matches",
+  },
+  {
+  icon:<FaBuilding />,
+  heading:"five star Hotel",
+  paragraph:"enjoy the ultimate in comfort and elegance at our curated selection of five-star hotels.",
+  },
+  {
+  icon:<HiGlobeAsiaAustralia />,
+  heading:"air ticketing",
+  paragraph:"enjoy the ultimate in comfort and elegance at our curated selection of five-star hotels.",
   },
   ]
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
-
+import Common from './Common'
+import { ServicesData } from '../../Data'
+import Image from 'next/image'
+import carImage from "/public/car.png"
+import blob from "/public/svg/blob-02.svg"
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className='relative xl:my-36'>
+      <div className="flex items-center justify-between lg:flex-row flex-col container mx-auto">
+        {/* text */}
+        <div className="">
+          <Common spanText={`how we work`} headingText={`we offer best services`} headingStyle={`font-semibold text-3xl`}/>
+
+        {/*  */}
+        <div className="flex flex-col">
+          {
+            ServicesData.map((item,index)=>{
+              return(
+                <div className="flex items-center gap-3 my-2" key={index}>
+                    <div className="p-5 bg-white shadow-lg rounded-lg text-2xl text-orangePrimary">
+                      {item.icon}
+                    </div>
+                    <div className="">
+                      <h4 className='text-xl font-semibold text-heading capitalize'>{item.heading}</h4>
+                      <p className="p">{item.paragraph}</p>
+                    </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+      {/* image */}
+      <div className="absolute right-[13rem]">
+        <Image src={carImage} alt='car'/>
+      </div>
+      {/* blob */}
+      <div className="bottom-0  top-10 right-0 -z-10 absolute">
+          <Image src={blob} alt='car image'/>
+      </div>
+      </div>
+    </section>
   )
 }
 
